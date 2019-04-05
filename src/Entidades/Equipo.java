6c007f9a140973378e78c4c24b8a764c2c889525
CristianAgudeloCalderon <cristian.1701322386@ucaldas.edu.co>
2019-04-05 17:34:24 -0500

    Se crea el metodo abstracto de AdicionarJugador, lista de jugadores, entrenador y sus respectivos Get() y Set()

--- src/Entidades/Equipo.java
+++ src/Entidades/Equipo.java
@@ -1,5 +1,29 @@
 package Entidades;
 
+import java.util.LinkedList;
+
 public abstract class Equipo {
+	protected LinkedList<Jugador> listaJugadores;
+	private Jugador entrenador;
+	
+	
+	public abstract boolean AdicionarJugador(Jugador jugador);
+
+
+	public Jugador getEntrenador() {
+		return entrenador;
+	}
+
+
+	public void setEntrenador(Jugador entrenador) {
+		this.entrenador = entrenador;
+	}
+
+
+	public LinkedList<Jugador> getListaJugadores() {
+		return listaJugadores;
+	}
+	
+	
 
 }
